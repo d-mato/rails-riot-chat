3fe4b2bf0139afb8c3dcba4a494b02e8ea26cb7f
Daiki Matoba <telnetstat@gmail.com>
2016-11-28 05:31:06 +0900

    Show auth info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  before_action do
+    puts "user_signed_in?: #{user_signed_in?}"
+    p current_user
+    puts "access_token: #{request.headers['X-Access-Token']}"
+  end
 end
